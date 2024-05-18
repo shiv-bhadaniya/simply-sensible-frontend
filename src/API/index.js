82df6b0f04bbe17cd3e42444ed1a303489655353
Shiv Bhadaniya <shivbhadaniya56@gmail.com>
2024-05-18 17:12:43 +0530

    refactor: APIs style

--- src/API/index.js
+++ src/API/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseAPI = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: process.env.REACT_APP_BACKEND_HOST,
     withCredentials: true,
 });
 
